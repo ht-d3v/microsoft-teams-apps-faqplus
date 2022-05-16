c5f1aadd4dc1c53db6ca8e307693e7ae8786eb47
Harry Traynor <harry@sb2.team>
2022-05-16 15:28:59 +0100

    Removed for error CS0117

--- Source/Microsoft.Teams.Apps.FAQPlusPlus.Common/Cards/UnrecognizedInputCard.cs
+++ Source/Microsoft.Teams.Apps.FAQPlusPlus.Common/Cards/UnrecognizedInputCard.cs
@@ -47,8 +47,8 @@ namespace Microsoft.Teams.Apps.FAQPlusPlus.Common.Cards
                             MsTeams = new CardAction
                             {
                                 Type = ActionTypes.MessageBack,
-                                DisplayText = Strings.AskAnExpertDisplayText,
-                                Text = Constants.AskAnExpert,
+                                DisplayText = Strings.AskAnExpertDisplayText
+//                                 Text = Constants.AskAnExpert,
                             },
                             UserQuestion = userQuestion,
                         },
